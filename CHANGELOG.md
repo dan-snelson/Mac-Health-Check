2c370f104fae340a33f5c12206c99173da8127ae
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2025-08-12 10:48:48 -0400

    2.2.0b8
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@
 - Add framework for different VPN clients and an internal VPN Client Check (Pull Request #16; thanks for another one, @HowardGMac!)
 - Addressed MHC does not show SF Symbols in the upper left corner - needs region check (Issue #21; thanks, @hbokh!)
 - Active IP Address section changes (Pull Request #24; thanks, Obi-@HowardGMac!)
+- Use zsh expansion in the `checkExternal` function to convert the results to lowercase so that the user doesn't have to match the case exactly in their results (Pull Request #25; thanks, @ScottEKendall!)
 
 ### 2.1.0 (24-Jul-2025)
 - Added an `operationMode` of "debug" to specifically enable swiftDialog debugging
--- Mac-Health-Check.zsh
+++ Mac-Health-Check.zsh
@@ -23,6 +23,7 @@
 #   - Add framework for different VPN clients and an internal VPN Client Check (Pull Request #16; thanks for another one, @HowardGMac!)
 #   - Addressed MHC does not show SF Symbols in the upper left corner - needs region check (Issue #21; thanks, @hbokh!)
 #   - Active IP Address section changes (Pull Request #24; thanks, Obi-@HowardGMac!)
+#   - Use zsh expansion in the `checkExternal` function to convert the results to lowercase so that the user doesn't have to match the case exactly in their results (Pull Request #25; thanks, @ScottEKendall!)
 #
 ####################################################################################################
 
@@ -37,7 +38,7 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin/
 
 # Script Version
-scriptVersion="2.2.0b7"
+scriptVersion="2.2.0b8"
 
 # Client-side Log
 scriptLog="/var/log/org.churchofjesuschrist.log"
